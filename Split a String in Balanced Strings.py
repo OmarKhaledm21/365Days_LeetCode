2dc97fb7d3db9ce1ae1fff65843a99983b042092
Omarkhaledm21 <Omarkhaledm21@gmail.com>
2023-01-22 13:32:14 +0200

    22/1

---
+++ Split a String in Balanced Strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        total = 0
+        rc = 0
+        lc = 0
+        for ch in s:
+            if ch == 'R':
+                rc += 1
+            else:
+                lc += 1
+            if rc == lc:
+                total += 1
+                rc = 0
+                lc = 0
+        return total
