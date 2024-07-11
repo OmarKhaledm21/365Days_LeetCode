0b3326bc9e68fb3bd1abf3605d591e128bb342ac
Omarkhaledm21 <omarkhaledm21@gmail.com>
2024-07-11 20:16:44 +0300

    11/07/2024

--- Valid Parentheses.java
+++ Valid Parentheses.java
@@ -1,54 +1,28 @@
-import java.util.Hashtable;
 import java.util.Stack;
 class Solution {
-    final Hashtable<String,String> expressions = new Hashtable<>();
-    public boolean isValid(String s) {
-        expressions.put("]","[");
-        expressions.put(")","(");
-        expressions.put("}","{");
+ 	public boolean isValid(String s) {
+		if (s == null || s.length() == 0)
+			return true;
 
-        Stack<String> stack = new Stack<>();
-        boolean exp = true;
-        for(int i=0; i<s.length(); i++){
-            String c = String.valueOf(s.charAt(i));
-            if(c.equals("]")  || c.equals(")") || c.equals("}")){
-                if(stack.isEmpty()){
-                    return false;
-                }
-                if(stack.peek().equals( expressions.get(c) ) ){
-                    stack.pop();
-                    exp = false;
-                    stack = validateStack(stack);
-                }
-            }
-            if(exp){
-                stack.add(c);
-            }
-            exp = true;
+		HashMap<Character, Character> map = new HashMap<>();
+		map.put(')', '(');
+		map.put('}', '{');
+		map.put(']', '[');
 
-        }
-        return stack.isEmpty();
-    }
+		Stack<Character> stack = new Stack<>();
 
-    public Stack<String> validateStack(Stack<String> stack){
-        if(!stack.isEmpty()){
-            boolean exp = true;
-            while (true){
-                String c = stack.pop();
-                if(c.equals("]")  || c.equals(")") || c.equals("}")){
-                    
-                    if(stack.peek().equals( expressions.get(c) ) ){
-                        stack.pop();
-                    }else{
-                        stack.add(c);
-                        break;
-                    }
-                }else{
-                    stack.add(c);
-                    break;
-                }
-            }
-        }
-        return stack;
-    }
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (map.containsKey(c)) {
+				char topElement = stack.empty() ? '#' : stack.pop();
+				if (topElement != map.get(c)) {
+					return false;
+				}
+			} else {
+				stack.push(c);
+			}
+		}
+		return stack.isEmpty();
+	}
+
 }
