33b83d83571e264b44bf577db1923c024bfb713d
Omarkhaledm21 <omarkhaledm21@gmail.com>
2024-08-20 15:09:18 +0300

    20/08/2024

--- Binary Tree Level Order Traversal.java
+++ Binary Tree Level Order Traversal.java
@@ -1,57 +1,57 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
 class Solution {
-    public static void main(String[] args) {
-        TreeNode treeNode = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-        new Solution().levelOrder(treeNode);
+    class Pair {
+        TreeNode node;
+        int lvl;
+
+        public Pair(TreeNode node, int lvl) {
+            this.node = node;
+            this.lvl = lvl;
+        }
     }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> parent = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        List<Integer> child;
+        List<List<Integer>> list = new ArrayList();
         if (root == null) {
-            return parent;
+            return list;
         }
-        queue.add(root);
+        Queue<Pair> queue = new LinkedList();
+        int currLvl = 0;
+        Pair p = new Pair(root, currLvl);
+        queue.add(p);
         while (!queue.isEmpty()) {
-            child = new ArrayList<>();
-            int n = queue.size();
-            for (int i = 0; i < n; i++) {
-                TreeNode current = queue.poll();
-                child.add(current.val);
-                if (current.left != null) {
-                    queue.add(current.left);
-                }
-                if (current.right != null) {
-                    queue.add(current.right);
-                }
+            Pair curr = queue.poll();
+            TreeNode currNode = curr.node;
+            currLvl = curr.lvl;
+
+            if (list.size() <= currLvl) {
+                list.add(new ArrayList());
             }
-            parent.add(child);
+            list.get(currLvl).add(currNode.val);
+
+            if (currNode.left != null) {
+                queue.add(new Pair(currNode.left, currLvl + 1));
+            }
+            if (currNode.right != null) {
+                queue.add(new Pair(currNode.right, currLvl + 1));
+            }
+
         }
-        return parent;
+
+        return list;
     }
 }
-
