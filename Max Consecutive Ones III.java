98693b7162424cc621c9ebbc6e8dda7592880f1e
Omarkhaledm21 <omarkhaledm21@gmail.com>
2025-01-11 18:54:20 +0200

    11/1/2025'

--- Max Consecutive Ones III.java
+++ Max Consecutive Ones III.java
@@ -1,20 +1,21 @@
-public static int longestOnes(int[] nums, int k) {
-        int maxWindow = 0;
-        int flipped = 0;
-        int i = 0;
-        int j = 0;
-        while (j < nums.length) {
-            if (nums[j] == 0) {
-                flipped++;
-            }
-            while (flipped > k) {
-                if (nums[i] == 0) {
-                    flipped--;
-                }
-                i++;
-            }
-            maxWindow = Math.max(maxWindow, j - i + 1);
-            j++;
-        }
-        return maxWindow;
-    }
+class Solution {
+	public static int longestOnes(int[] nums, int k) {
+		int max = 0;
+		int flips = 0;
+		int j = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				flips++;
+			}
+			while (flips > k) {
+				if (nums[j] == 0) {
+					flips--;
+				}
+				j++;
+			}
+			max = Math.max(max, i - j + 1);
+		}
+
+		return max;
+	}
+}
